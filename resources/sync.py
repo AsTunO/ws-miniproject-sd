ba8a22c1cfba45b28e04d4a55d5978e09a397c0e
Júlio Cesar <juliocesar27072004@outlook.com>
2024-06-04 18:18:40 -0300

    Delete resources directory

--- resources/sync.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-from utils.sync_utils import sincronizar_dados
-
-class SyncResource(Resource):
-    def post(self):
-        sincronizar_dados()
-        return {'message': 'Sincronização concluída com sucesso'}, 200
